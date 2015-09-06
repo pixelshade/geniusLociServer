582c03854b330a24987f3b6ccd1bf3696a92c032
pixelshade <psuldo@gmail.com>
2015-09-06 23:59:16 +0200

    added just small query snippet for finding within range, for later use

--- api/controllers/GhostController.js
+++ api/controllers/GhostController.js
@@ -9,7 +9,7 @@ module.exports = {
 
     'findnearposition': function(req, res){
 //        ghost.find({"loc": SON([("$near", [3, 6]), ("$maxDistance", 100)])}).limit(3);
-
+//          db.places.find({"loc": {"$within": {"$center": [[0, 0], 6]}}}):
     }
 };
 
