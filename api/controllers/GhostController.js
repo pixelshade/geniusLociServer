176ddc40f8d80b15132d821a0581873f0d9ed532
pixelshade <psuldo@gmail.com>
2015-09-06 23:55:35 +0200

    ghost api controller

---
+++ api/controllers/GhostController.js
@@ -0,0 +1,15 @@
+/**
+ * GhostController
+ *
+ * @description :: Server-side logic for managing ghosts
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+    'findnearposition': function(req, res){
+//        ghost.find({"loc": SON([("$near", [3, 6]), ("$maxDistance", 100)])}).limit(3);
+
+    }
+};
+
