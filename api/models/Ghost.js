ce4b28d078f1fef72334ac1e2dab27c319aa0001
pixelshade <psuldo@gmail.com>
2015-09-06 23:34:52 +0200

    ghost api, zabudnute subory

---
+++ api/models/Ghost.js
@@ -0,0 +1,25 @@
+/**
+* Ghost.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+       name: {
+           type: 'string'
+       },
+
+       content: {
+           type: 'string',
+           required: true
+       },
+      loc : {
+          type: '2dsphere'
+      }
+
+  }
+};
+
